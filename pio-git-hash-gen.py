d21bee892eb146b9bea1bfe4fbeb7c27b3031a33
mathbrook <32876429+mathbrook@users.noreply.github.com>
2024-01-03 01:24:12 -0500

    ok now it actually gets the project hash lmfao

--- pio-git-hash-gen.py
+++ pio-git-hash-gen.py
@@ -2,9 +2,10 @@ Import("projenv")
 import subprocess
 
 def get_firmware_specifier_build_flag():
-    build_version  = subprocess.run(['git', 'rev-parse','--short', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    path = str(projenv["PROJECT_DIR"])
+    build_version  = subprocess.run(['git','-C',path, 'rev-parse','--short', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')
     build_version.strip()
-    print((build_version))
+    print("VERSION: " + (build_version))
     return int(build_version,16)
 
 fw_flag=get_firmware_specifier_build_flag()

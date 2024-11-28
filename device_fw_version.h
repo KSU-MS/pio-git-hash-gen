42d377b8161f4e01b33c9a0922dd25e6b84072aa
Ben Hall <rcmast3r1@gmail.com>
2024-11-28 16:31:16 -0500

    added helper function into header

--- device_fw_version.h
+++ device_fw_version.h
@@ -1,6 +1,9 @@
-#ifndef __KSU_DEVICE_STATUS_H__
-#define __KSU_DEVICE_STATUS_H__
+#ifndef __DEVICE_FW_VERSION__
+#define __DEVICE_FW_VERSION__
 #include <stdint.h>
+#include <cstddef>
+#include <stdio.h>
+
 #ifndef AUTO_VERSION
 #warning "AUTO_VERSION was not defined by the generator!"
 #define AUTO_VERSION 0xdeadbeef
@@ -15,11 +18,18 @@
 #endif
 
 // ideally little endian bc teensy
-typedef struct device_status_t
+struct device_status_t
 {
     const uint32_t firmware_version = AUTO_VERSION;
-    uint16_t on_time_seconds = 0;
     const bool project_on_main_or_master = FW_PROJECT_IS_MAIN_OR_MASTER;
     const bool project_is_dirty = FW_PROJECT_IS_DIRTY;
-} device_status_t;
+};
+
+std::array<char, 8> convert_version_to_char_arr(uint32_t hex_ver)
+{
+    std::array<char, 8> ver_str;
+    (void)snprintf(ver_str.data(), 8, "%x", hex_ver);
+    return ver_str;
+}
+
 #endif
